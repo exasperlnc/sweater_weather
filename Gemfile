6afd3e821bde917c773db9e630c522a2ae8859ab
Logan Cole <emtlogancole@gmail.com>
2023-06-12 18:38:51 -0500

    feat: password digest for user

--- Gemfile
+++ Gemfile
@@ -47,6 +47,9 @@ gem 'jsonapi-serializer'
 #pry gem
 gem 'pry-rails'
 
+#encription gem
+gem 'bcrypt'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'validations' do
     it { should validate_presence_of :email }
-    it { should validate_presence_of :password}
-    it { should validate_presence_of :password_confirmation}
+    it { should validate_presence_of :password_digest }
+    it { should have_secure_password }
     it { should validate_uniqueness_of :email}
   end
 end
