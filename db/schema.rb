f700a3a64b885b4533e9eadfdb919052ee67dc8c
Logan Cole <emtlogancole@gmail.com>
2023-06-13 08:41:50 -0500

    chore: clean up database

--- db/schema.rb
+++ db/schema.rb
@@ -14,14 +14,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_12_233646) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "books", force: :cascade do |t|
-    t.string "title"
-    t.string "publisher"
-    t.string "isbn"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "password"
